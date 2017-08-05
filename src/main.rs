0ca01f56b54674e7925f558bedac00818a055fed
beaubbe <gabriel.couvrette@gmail.com>
2017-08-05 08:13:00 -0400

    Extracted tests in their own file.

--- src/main.rs
+++ src/main.rs
@@ -35,36 +35,4 @@ fn extract_topics_from_args_vec(args: &Vec<String>) -> Result<(&str, &str), &str
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn extract_topics_from_args_with_empty_vec_should_err() {
-        let empty_vec = vec![];
-        assert_eq!(extract_topics_from_args_vec(&empty_vec), Err("Error reading arguments."));
-    }
-
-    #[test]
-    fn extract_topics_from_args_with_only_one_item_should_err() {
-        let vec = vec!["0".to_owned()];
-        assert_eq!(extract_topics_from_args_vec(&vec), Err("Missing argument: Start topic"));
-    }
-
-    #[test]
-    fn extract_topics_from_args_with_only_two_item_should_err() {
-        let vec = vec!["0".to_owned(), "1".to_owned()];
-        assert_eq!(extract_topics_from_args_vec(&vec), Err("Missing argument: End topic"));
-    }
-
-    #[test]
-    fn extract_topics_from_args_with_three_items_should_ok() {
-        let vec = vec!["0".to_owned(), "1".to_owned(), "2".to_owned()];
-        assert_eq!(extract_topics_from_args_vec(&vec), Ok(("1", "2")));
-    }
-
-    #[test]
-    fn extract_topics_from_args_with_too_many_items_should_err() {
-        let vec = vec!["0".to_owned(), "1".to_owned(), "2".to_owned(), "3".to_owned(), "4".to_owned()];
-        assert_eq!(extract_topics_from_args_vec(&vec), Err("Too many arguments"));
-    }
-}
+mod tests;
