86ab4db948532af9d634ead14eba1d5bc8f6f439
beaubbe <gabriel.couvrette@gmail.com>
2017-08-12 23:18:42 -0400

    Re-indent story_builder

--- src/story_builder/story_builder/mod.rs
+++ src/story_builder/story_builder/mod.rs
@@ -17,8 +17,7 @@ impl <'a> StoryBuilder<'a>  {
     }
 
     pub fn build_story(&self, start_topic: &str , end_topic: &str) -> Result<String, String> {
-        let end_topic = &end_topic.to_lowercase();
-                   // If one of the topics is an empty string, do not try to make a story out of it.
+        // If one of the topics is an empty string, do not try to make a story out of it.
         if start_topic == "" {
             return Err("Missing start topic.".to_owned());
         }
@@ -26,60 +25,69 @@ impl <'a> StoryBuilder<'a>  {
             return Err("Missing end topic.".to_owned());
         }
 
-                   // If both topics are the same,
-                   // there is no point in trying to figure out the story.
+        // If both topics are the same,
+        // there is no point in trying to figure out the story.
         if start_topic == end_topic {
             return Err("No story to build; same start and end topics.".to_owned());
         }
 
-                   // Load the first article
+        // Load the first article
         let start_article = match self.article_provider.get(start_topic) {
             Some(start_article) => start_article,
             None => {return Err(self.build_suggestions_msg(start_topic));},
         };
 
-                   // Load the end article
+        // Load the end article
         let end_article = match self.article_provider.get(end_topic) {
             Some(end_topic) => end_topic,
             None => {return Err(self.build_suggestions_msg(end_topic));},
         };
 
-                   /* To build a story, we need to build a tree starting at the start_article
-                      node and going down in a "breadth-first" way; that way, once we find
-                      the end note, we know it is the shortest path to it. Also, going depth-first
-                      will be impossibly long to complete sincethe depth of the wikipedia
-                      article tree is almost infinite. */
-                   /* We look for a paragraph that holds a reference to our end topic
-                      somewhere in the last level we fetched: */
+        /* To build a story, we need to build a tree starting at the start_article
+           node and going down in a "breadth-first" way; that way, once we find
+           the end note, we know it is the shortest path to it. Also, going depth-first
+           will be impossibly long to complete sincethe depth of the wikipedia
+           article tree is almost infinite. */
+        /* We look for a paragraph that holds a reference to our end topic
+           somewhere in the last level we fetched: */
         let mut last_level: Vec<Rc<ArticleNode>> = vec![Rc::new(ArticleNode::new(start_article))]; // starts with start article
         for i in 0..self.max_depth { // To prevent overloading the system, stop after X level deep
-                       // Start by loading the next level of articles:
-           if i > 0 {
-               // Any other iteration: go one level deeper:
-               let mut current_level = vec![];
-               for rc_article_node in last_level.iter() {
-                       /* Iterate on each paragraph of this article, then map on it to
-                          get the article for each related topic in it. */
-               let rc_local = rc_article_node.clone();
-               let article = rc_local.deref();
-               for paragraph in article.get_paragraphs().iter() {
-                   for topic in paragraph.topics.iter() {
-                       let mut new_node = ArticleNode::new((&self).article_provider.get(topic).unwrap());
-                       new_node.attach_to(rc_local.clone(), &paragraph.text);
-                       current_level.push(Rc::new(new_node));
-                       }
-                   }
-               }
-               last_level = current_level; // Replace the previous level with this level.
-           }
+            // Start by loading the next level of articles:
+            if i > 0 {
+                // Any other iteration: go one level deeper:
+                let mut current_level = vec![];
+                for rc_article_node in last_level.iter() {
+                    /* Iterate on each paragraph of this article, then map on it to
+                       get the article for each related topic in it. */
+                    let rc_local = rc_article_node.clone();
+                    let article = rc_local.deref();
+                    for paragraph in article.get_paragraphs().iter() {
+                        // First: spawn all threads first
+                        let mut threads: Vec<JoinHandle<Article>> = Vec::new();
+                        for topic in paragraph.topics.iter() {
+                            threads.push(thread::spawn(|| {
+                                (&self).article_provider.get(topic.to_owned()).unwrap()
+                            }));
+                        }
+                        // Then, join them up one at a time.
+                        for topic in paragraph.topics.iter() {
 
-           for article_node in last_level.iter() {
-               if let Some(text) = StoryBuilder::find_text_for_topic_in_article(article_node.deref().deref().borrow(), end_topic) {
-                   // Found the topic. Format and return.
-                   return Ok(StoryBuilder::build_final_text(article_node.clone(), text));
-               }
-           }
-       }
+                            let mut new_node = ArticleNode::new();
+                            new_node.attach_to(rc_local.clone(), &paragraph.text);
+                            current_level.push(Rc::new(new_node));
+                        }
+                    }
+                }
+                last_level = current_level; // Replace the previous level with this level.
+            }
+
+            for article_node in last_level.iter() {
+                if let Some(text) = StoryBuilder::find_text_for_topic_in_article(article_node.deref().deref().borrow(), end_topic) {
+                    // Found the topic. Format and return.
+                    return Ok(StoryBuilder::build_final_text(article_node.clone(), text));
+                }
+            }
+        }
 
 
         Err(format!("Reached depth of <{}> without finding <{}>. Stopping search.", self.max_depth, end_topic).to_owned())
@@ -88,7 +96,7 @@ impl <'a> StoryBuilder<'a>  {
     fn build_suggestions_msg(&self, topic: &str) -> String {
         let mut msg = String::from(format!("Cannot find wikipedia article for <{}>, try one of the following suggestions:\r\n", topic));
         for sugg in self.article_provider.search(topic) {
-        msg.push_str(&format!("- {}\r\n", &sugg));
+            msg.push_str(&format!("- {}\r\n", &sugg));
         }
 
         msg
@@ -96,10 +104,10 @@ impl <'a> StoryBuilder<'a>  {
 
     fn find_text_for_topic_in_article<'b>(article: &'b Article, topic: &str) -> Option<&'b str> {
         if let Some(paragraph) = article.get_paragraphs().iter().find(|par| {
-                   // if any of the topics in the paragraph is <end>, return it.
-            par.topics.iter().any(|t| {&t.to_lowercase() == topic})
-            }) {
-                   // We found the paragraph; return it directly.
+            // if any of the topics in the paragraph is <end>, return it.
+            par.topics.iter().any(|t| {t == topic})
+        }) {
+            // We found the paragraph; return it directly.
             return Some(&paragraph.text);
         } else {
             None
@@ -119,14 +127,14 @@ impl <'a> StoryBuilder<'a>  {
         loop {
             node = match node.parent() {
                 Some(rc) => {
-                        {
+                    {
                         let n: &ArticleNode = rc.borrow();
                         if let Some(n_text) = n.text() {
                             text.push_str(&format!("{}\r\n", n_text));
                         }
-                        }
+                    }
                     rc
-                    },
+                },
                 _ => break,
             }
         }
