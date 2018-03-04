4e645ce903b17e08f99a8a1a445d69bd3719a66e
beaubbe <gabriel.couvrette@gmail.com>
2018-03-04 09:56:39 -0500

    Only index actual articles (namespace 0) from the Wiki XML Data file.

--- src/xml_wiki_parser/mod.rs
+++ src/xml_wiki_parser/mod.rs
@@ -6,13 +6,21 @@ use self::xml::reader::*;
 /// This function takes a Reader `data_source`, consumes it,
 /// indexes it and outputs the result in the `index_out` Writer.
 pub fn generate_index<R: Read + Seek>(data_source: R, index_out: &mut Write) {
+    println!("Starting to index file...");
     let mut xml_reader = EventReader::new(data_source);
     // The XML file is made of lots of tags, but we only want to keep <page> events.
     while let Some(page_pos) = skip_until_page_start(&mut xml_reader) {
         if let Some(title) = extract_next_title(&mut xml_reader) {
-            index_out.write(format!("{},{}\r\n", page_pos, title).as_bytes());
+            if let Some(ns) = extract_next_namespace(&mut xml_reader) {
+                // Only consider namespace 0 (default articles)
+                // Skip the others (files, templates, gadgets, etc..)
+                if ns == "0" {
+                    index_out.write(format!("{},{}\r\n", page_pos, title).as_bytes());
+                }
+            }
         }
     }
+    println!("Done reading!");
 }
 /// Iterate over all XmlEvents in the `reader` until a <page> is found.
 /// If an error occurs while reading, or the end of the file is reached,
@@ -46,6 +54,29 @@ fn extract_next_title<R: Read>(reader: &mut EventReader<R>) -> Option<String> {
                 if let Ok(XmlEvent::Characters(title)) = reader.next() {
                     // Extract the title and return it:
                     return Some(title);
+                } else {
+                    // Did not get a title; unexpected !! Return nothing for now.
+                    // TODO: Return proper errors.
+                    return None;
+                }
+            }
+        }
+    }
+    // Reached end of file (or got an error) while looking for <title>, return nothing:
+    return None;
+}
+
+fn extract_next_namespace<R: Read>(reader: &mut EventReader<R>) -> Option<String> {
+    // Read all events until we get a title:
+    while let Ok(event) = reader.next() {
+        // If the event is a start element:
+        if let XmlEvent::StartElement{name, ..} = event {
+            // And it is for page:
+            if name.local_name == "ns" {
+                // Read an extra tag; it is the content of the namespace tag:
+                if let Ok(XmlEvent::Characters(ns)) = reader.next() {
+                    // Extract the title and return it:
+                    return Some(ns);
                 } else {
                     // Did not get a title; unexpected !! Return nothing for now.
                     // TODO: Return proper errors.
