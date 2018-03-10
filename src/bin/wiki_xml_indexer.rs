ef86edab754b2146052105aba63693377d766a35
beaubbe <gabriel.couvrette@gmail.com>
2018-03-09 21:11:02 -0500

    Add "auto-Resume" to the indexer

--- src/bin/wiki_xml_indexer.rs
+++ src/bin/wiki_xml_indexer.rs
@@ -1,15 +1,19 @@
 extern crate wikistory;
 use wikistory::xml_wiki_parser::generate_index as generate_index;
 use std::fs::File;
-use std::io::{BufReader};
+use std::fs::OpenOptions;
 
 fn main() {
     // 1. Open XML data file to read from:
-    let file_in = File::open("./data/enwiki-20170820-pages-articles.xml").expect("File not found.");
+    let xml_file = File::open("./data/enwiki-20170820-pages-articles.xml").expect("File not found.");
 
-    // 2. Create file to write to:
-    let mut file_out = File::create("./data/index.csv").expect("Unable to create index file.");
-
+    // 2: Open the index output file:
+    let index_file = OpenOptions::new()
+                        .read(true)
+                        .write(true)
+                        .create(true) // Or create a new file if it does not exist
+                        .open("./data/index.csv")
+                        .expect("Unable to create index file.");
     // 3. Index the file:
-    generate_index(BufReader::new(file_in), &mut file_out);
+    generate_index(xml_file, index_file);
 }
--- src/xml_wiki_parser/mod.rs
+++ src/xml_wiki_parser/mod.rs
@@ -1,26 +1,42 @@
 extern crate xml;
 
-use std::io::{Read, Write, Seek, SeekFrom};
+use std::io::{BufReader, Read, Write, Seek, SeekFrom};
 use self::xml::reader::*;
 
 /// This function takes a Reader `data_source`, consumes it,
 /// indexes it and outputs the result in the `index_out` Writer.
-pub fn generate_index<R: Read + Seek>(data_source: R, index_out: &mut Write) {
-    println!("Starting to index file...");
-    let mut xml_reader = EventReader::new(data_source);
+pub fn generate_index<R: Read + Seek, W: Read + Write + Seek>(mut data_source: R, mut index_out: W) {
+    /* To allow resuming indexation process, read the last indexed position from the index and
+       seek to it: */
+    let seek_resume = get_last_index_position(&mut index_out);
+    let mut must_skip_one = false;
+    if seek_resume > 0 {
+        println!("Seeking to {}", seek_resume);
+        must_skip_one = true;
+    }
+    data_source.seek(SeekFrom::Start(seek_resume));
+    println!("Indexing...");
+    let mut xml_reader = EventReader::new(BufReader::new(data_source));
+    // Skip the next article since it was already in the index
+    if (must_skip_one) {
+        skip_until_page_start(&mut xml_reader);
+        xml_reader.next();
+    }
     // The XML file is made of lots of tags, but we only want to keep <page> events.
+    // Skip to the next article (<page>) start:
     while let Some(page_pos) = skip_until_page_start(&mut xml_reader) {
+        // Then, read the title of that article:
         if let Some(title) = extract_next_title(&mut xml_reader) {
+            // Also extract the namespace:
             if let Some(ns) = extract_next_namespace(&mut xml_reader) {
                 // Only consider namespace 0 (default articles)
                 // Skip the others (files, templates, gadgets, etc..)
                 if ns == "0" {
-                    index_out.write(format!("{},{}\r\n", page_pos, title).as_bytes());
+                    index_out.write(format!("{},{}\r\n", page_pos, title).as_bytes()).expect("Cannot write to index file. Aborting.");
                 }
             }
         }
     }
-    println!("Done reading!");
 }
 /// Iterate over all XmlEvents in the `reader` until a <page> is found.
 /// If an error occurs while reading, or the end of the file is reached,
@@ -42,7 +58,6 @@ fn skip_until_page_start<R: Read + Seek>(reader: &mut EventReader<R>) -> Option<
     // No more pages. Return nothing.
     return None;
 }
-
 fn extract_next_title<R: Read>(reader: &mut EventReader<R>) -> Option<String> {
     // Read all events until we get a title:
     while let Ok(event) = reader.next() {
@@ -87,6 +102,32 @@ fn extract_next_namespace<R: Read>(reader: &mut EventReader<R>) -> Option<String
     }
     // Reached end of file (or got an error) while looking for <title>, return nothing:
     return None;
+}
+
+fn get_last_index_position<W: Read + Write + Seek>(index_in: &mut W) -> u64 {
+    // Go back 4k characters and read the lines:
+    const CHUNK_SIZE: usize = 4096;
+    let start_pos = index_in.seek(SeekFrom::End(-1*CHUNK_SIZE as i64)).expect("Cannot seek in index file. Aborting.");
+    let mut buf = String::with_capacity(CHUNK_SIZE);
+    index_in.read_to_string(&mut buf).expect("Unable to read index file. Aborting.");
+    let lines: Vec<&str> = buf.split("\r\n").filter(|x| x.len() > 0 /* Ignore empty lines */).collect();
+    let line_count = lines.len();
+    if line_count == 0 {
+        return 0; // There was no content in the file, return 0.
+        // If we have at least 2 lines, it means the first line might be mangled but the second (last) line is complete.
+        // Or, if the start position was the start of the file, and we read a line, then we have the only line in the file.
+        // In this case, we can process it.
+    } else if (line_count >= 2) || (line_count == 1 && start_pos == 0) {
+        // The lines are built as: SeekIndex,ArticleName
+        // Split on comma, parse the first part and return it.
+            println!("{:?}", lines[line_count - 1]);
+        return lines[line_count - 1]
+                .split(',')
+                .next().expect("Index file does not respect the correct file format (Must be comma-separated.)")
+                .parse().expect("Cannot parse index as a valid number.");
+    } else {
+        panic!("We did not get a full line by reading the last chunk of the index file (TODO: Implement this!). Aborting.");
+    }
 }
 
 #[cfg(test)]
